c8dda46f84d686f848cfb34072df83941204a034
gore-gh <45643492+gore-gh@users.noreply.github.com>
2018-12-10 01:34:17 +0000

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 console.log("Loading bot...");
 
+// Load required modules
 const SteamUser = require("steam-user");
 const SteamTotp = require("steam-totp");
 const Gamedig = require('gamedig');
@@ -9,20 +10,24 @@ var util = require('util');
 
 const client = new SteamUser();
 
-var lastUser = "";
+var consoleBlack = "\x1b[30m%s\x1b[0m";
+var consoleRed = "\x1b[31m%s\x1b[0m";
+var consoleGreen = "\x1b[32m%s\x1b[0m";
+var consoleYellow = "\x1b[33m%s\x1b[0m";
+var consoleBlue = "\x1b[34m%s\x1b[0m";
+var consoleMagenta = "\x1b[35m%s\x1b[0m";
+var consoleCyan = "\x1b[36m%s\x1b[0m";
+var consoleWhite = "\x1b[37m%s\x1b[0m";
+var lastUser;
 
-var dbServers = [
-    {ip: "79.137.78.241:27015", name: "Akod [Advanced]", players: ""},
-    {ip: "137.74.170.69:27015", name: "Pandemic [Advanced]", players: ""},
-    {ip: "176.57.142.80:27015", name: "Panda #19", players: ""},
-    {ip: "193.192.59.191:27045", name: "Panda #04", players: ""},
-    {ip: "85.10.231.142:27015", name: "STD", players: ""},
-];
-
-var commands = [
+var greetings = [
     "hi",
-    "dump"
+    "hello",
+    "hey",
 ]
+var commands = [
+    "dump"
+];
 
 const logOnOptions = {
     accountName: cfg.username,
@@ -30,118 +35,106 @@ const logOnOptions = {
     twoFactorCode: SteamTotp.generateAuthCode(cfg.sharedSecret)
 };
 
+// Log the bot in
 client.logOn(logOnOptions);
 
 client.on("loggedOn", () => {
-    console.log("Bot logged in.\n");
+
+    console.log(consoleGreen, "\nBot logged in successfully!\n");
+
     client.setPersona(SteamUser.Steam.EPersonaState.Online);
-    client.gamesPlayed("");
+    client.gamesPlayed(cfg.gameToPlay);
+
 });
 
+// Check how many friends are online
 client.on("friendsList", function() {
 
     console.log(Object.keys(client.myFriends).length + " Friends online.");
 
 }); 
 
+// Check if we have any messages
+client.on("offlineMessages", function(count, friends) {
+
+    if (friends != null) {
+
+        for (var i = 0; i < friends.length; i++) {
+
+            var user = friends[i];
+
+            client.getChatHistory([user], function(success, messages)  {
+
+                console.log(consoleYellow, "\nUnread messages from " + user + ":\n")
+
+                for (var j = 0; j < messages.length; j++) {
+
+                    if (messages[j].unread) {
+                        
+                        console.log(user + " @ " + messages[j].timestamp.toLocaleString() + "\n" + messages[j].message + "\n");
+
+                    }
+
+                }
+    
+            });
+
+        }
+
+    }
+
+});
+
+// Handle messages from friends
 client.on("friendMessage", function(steamID, message) {
     
     var msg = "";
 
-    /*if (steamID == "76561198058950254") {
-        client.chatMessage(lastUser, "[GORE]: " + message)
+    if (steamID == "76561198058950254") {
+        console.log(message);
+        (lastUser == null) ? console.log("No last user to message") : client.chatMessage(lastUser, "[GORE]: " + message);
     }
     else {
         client.getPersonas([steamID], function(personas) {
             var persona = personas[steamID];
             var name = persona ? persona.player_name : ("[" + steamID + "]");
-            client.chatMessage("76561198058950254", "[" + name + "]: " + message);
+            client.chatMessage(cfg.ownerSteamID, "[" + name + "]: " + message);
             console.log("[" + name + "]: " + message);
             lastUser = steamID;
         });
-    }*/
+    }   
 
     switch(message) {
-        case commands[0]:
+        case greetings[0]:
+        case greetings[1]:
+        case greetings[2]:
             client.chatMessage(steamID, "Hello");
-            break;
-        case commands[1]:   
-            for (var i = 0; i < dbServers.length; i++) {
-                msg = msg + "Server #" + i + ": " + dbServers[i].name + "\n";
-            } 
-            client.chatMessage(steamID, msg + "\n");
-            break;
+            break;  
+
         case "helpppppppppppp":
             for (var i = 0; i < commands.length; i++) {
                 client.chatMessage(steamID, "'" + commands[i] + "',\n");
             }  
             break;
+
         default: 
-            //client.chatMessage(steamID, "Message logged.");
             break;
     }
 
-});
+}); 
 
+// Handling friend requests
 client.on('friendRelationship', function(sid, relationship) {
-    if (relationship == SteamUser.EFriendRelationship.RequestRecipient) {
+
+    console.log("Friend request from SteamID: " + sid);
+
+    if (relationship == SteamUser.EFriendRelationship.RequestRecipient && cfg.autoAccept == "true") {
+
         client.addFriend(sid);
-        client.chatMessage("76561198058950254", "[" + sid + "] has sent a friend request to me");
-        client.chatMessage(sid, "Hello")
+        console.log("Accepted friend request from SteamID: " + sid);
+        client.chatMessage(cfg.ownerSteamID, "[" + sid + "] has sent a friend request to me");
+        client.chatMessage(sid, "Hello, to see a list of commands type help")
+
     }
-	console.log("Friend request from " + sid);
+
 });
-
-
-
-    /*
-        // Gamedig Query
-        Gamedig.query({
-            type: 'tf2',
-            host: '79.137.78.241',
-            port: '27015'
-        },
-        function(e,state) {
-            if(e) {
-                console.log("Server is offline");
-            }
-            else {
-                console.log(state);
-                fs.writeFile("pastebin.txt", util.inspect(state), function(err) {
-
-                    if(err) {
-                        return console.log(err);
-                    }
-                
-                    console.log("The file was saved!");
-                }); 
-            }
-        });
-
-        var filterConditions = {
-    app_id: 440,  
-    region_code: 0x03,
-    max_servers: 1,
-    filter_text: "\name_match\akod"
-}
-
-        // Pastebin post
-        var PastebinAPI = require('pastebin-js'),
-            pastebin = new PastebinAPI({
-            'api_dev_key' : 'dce8221c3a448506a68544b451bd836a',
-            'api_user_name' : 'gore_bot',
-            'api_user_password' : 'epicdudeman64'
-        });
- 
-        pastebin 
-            .createPasteFromFile("pastebin.txt", "Raw Dump | " + toQuery + " | " + currentTime, null, 1, "N")
-            .then(function (data) {
-                // we have succesfully pasted it. Data contains the id
-                console.log(data);
-                client.chatMessage(steamID, data);
-            })
-            .fail(function (err) {
-                console.log(err);
-            });
-
-    }*/
